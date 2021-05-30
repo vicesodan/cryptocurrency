f1b54a722a208e284c6b96908b62be2f869f22cb
vicesodan <vices.sodan@gmail.com>
2021-05-30 23:03:22 +0200

    2nd commit

--- rpcbitcoinx.py
+++ rpcbitcoinx.py
@@ -5,6 +5,7 @@ from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
 from datetime import datetime
 import matplotlib.pyplot as plt
 from tkinter import *
+from tkinter import scrolledtext
 from PIL import ImageTk, Image 
 
 
@@ -13,9 +14,14 @@ canvas.geometry("1000x600")
 canvas.title("BitcoinStats")
 canvas.iconbitmap('C:/Users/vices/Desktop/bitcoin.ico')
 
+rpc_connection = AuthServiceProxy("http://%s:%s@blockchain.oss.unist.hr:8332"%(rpc_user, rpc_password))
+
+def clearFrame(widget):
+    widget.forget()
 
 def trenutna_cijena():
-    url="https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD,JPY,EUR"
+    
+    url="https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD,JPY,EUR&api_key=06efcda2677d688ac20177063ed6af2a3e341d19db249029ec66e7072c6f932c"
     response = requests.get(url).json()
     price = response["USD"]
     time=datetime.now().strftime("%H:%M:%S")
@@ -28,13 +34,24 @@ def trenutna_cijena():
     labelPrice.config(text = str(price) + "$")
     labelTime.config(text = "Vrijeme: " + time)
     
-    canvas.after(1000, trenutna_cijena)
+    canvas.after(600, trenutna_cijena)
 
 def blockinfo():
 
-    rpc_connection = AuthServiceProxy("http://%s:%s@blockchain.oss.unist.hr:8332"%(rpc_user, rpc_password))
     best_block_hash = rpc_connection.getbestblockhash()
-    info = Label(canvas, text=(rpc_connection.getblock(best_block_hash))).pack()
+    info=rpc_connection.getblock(best_block_hash)
+    labelTitle.config(text = "Best block hash", font = f1)
+    labelTime.config(text = 'Hash = ' + info['hash'] + '\nMerkleroot = ' + str(info['merkleroot'] + '\nVersion = ' + str(info['version'])))
+
+def transactions():
+
+    labelPrice.config(text = "Popis transakcija:")
+    scrText=scrolledtext.ScrolledText(canvas, wrap=WORD)
+    scrText.pack()
+
+    trans = rpc_connection.getrawmempool()
+    scrText.insert('insert', trans) 
+    scrText.config(state=DISABLED)
 
 def graf():
     #PRIMJER NEKOG GRAFA
@@ -44,8 +61,8 @@ def graf():
     y = [2,4,1,5,2,6]
     
     # plotting the points 
-    plt.plot(x, y, color='green', linestyle='dashed', linewidth = 3,
-             marker='o', markerfacecolor='blue', markersize=12)
+    plt.plot(x, y, color='red', linestyle='dashed', linewidth = 3,
+             marker='o', markerfacecolor='red', markersize=12)
     
     # setting x and y axis range
     plt.ylim(1,8)
@@ -57,14 +74,17 @@ def graf():
     plt.ylabel('y - axis')
     
     # giving a title to my graph
-    plt.title('Some cool customizations!')
+    plt.title('Bitcoin price')
     
     # function to show the plot
     plt.show()
 
+
+
 f1=("poppins", 24, "bold")
 f2=("poppins", 22, "bold")
 f3=("poppins", 18, "normal")
+
 
 labelTitle = Label(canvas, font=f1)
 labelTitle.pack(pady=20)
@@ -75,20 +95,18 @@ labelPrice.pack(pady=20)
 labelTime = Label(canvas, font=f3)
 labelTime.pack(pady=20)
 
-labelInfo = Label(canvas, font=f3)
-labelInfo.pack(pady=20)
-
 my_menu = Menu(canvas)
 canvas.config(menu=my_menu)
 
 pocetna = Menu(my_menu)
 blockchain = Menu(my_menu)
+popist = Menu(my_menu)
 ggraf = Menu(my_menu)
 
 my_menu.add_cascade(label="Cijena", command=trenutna_cijena)
 my_menu.add_cascade(label="BlockInfo", command=blockinfo)
+my_menu.add_cascade(label="Popis transakcija", command=transactions)
 my_menu.add_cascade(label="Graf", command=graf)
-
 
 
 canvas.mainloop()
